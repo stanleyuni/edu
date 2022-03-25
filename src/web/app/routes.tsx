7a1966a47e97a01844fcdfb895bc2c0f7be72b2b
Jake Walklate <jakewalklate@gmail.com>
2022-03-25 13:40:08 +1100

    landing

--- src/web/app/routes.tsx
+++ src/web/app/routes.tsx
@@ -9,17 +9,14 @@ import {
 } from 'react-router-native';;
 
 // App components
-import Login from './views/login';
 import Landing from './views/landing';
-import Main from './views/main';
 
 const AppRoutes = ({ theme, logged_in }) => {
     return (
       <Routes>
-        <Route path='/' element={logged_in ? <Navigate to='/home'/> : <Navigate to='/landing' />} />
+        {/* <Route path='/' element={logged_in ? <Navigate to='/home'/> : <Navigate to='/landing' />} /> */}
+        <Route path='/' element={<Landing theme={theme} logged_in={logged_in} />} />
         <Route path='/landing' element={<Landing theme={theme} logged_in={logged_in} />} />
-        <Route path='/login' element={<Login theme={theme} logged_in={logged_in} />} />
-        <Route path='/home' element={<Main theme={theme} logged_in={logged_in} />} />
         <Route element={<Text theme={theme}>There's nothing here!</Text>} />
       </Routes>
     );
--- src/web/app/views/app-bar/index.tsx
+++ src/web/app/views/app-bar/index.tsx
@@ -25,87 +25,15 @@ const AppBar = ({ theme, logged_in }) => {
       flexWrap: 'wrap',
     },
   });
-
-  const ref = useRef({
-    dispatch: useDispatch(),
-    store: useStore()
-  });
-
-  const [state, setState] = useState(() => {
-    ref.current.dispatch({ type: 'notifications/set_notifications' });
-    let data = ref.current.store.getState();
-    return {
-      ...data['notifications']
-    }
-  });
-
+ 
   const nav = useNavigate();
   const loc = useLocation();
-  const is_landing = (loc.pathname.indexOf('/landing') >= 0);
-  const is_login = (loc.pathname.indexOf('/login') >= 0);
-  const is_home = (loc.pathname.indexOf('/home') >= 0);
-
-  function toggleNotifications() {
-    ref.current.dispatch({ type: 'notifications/toggle_notifications' });
-  }
-
-  ref.current.store.subscribe(() => {
-    const data = ref.current.store.getState().notifications;
-    setState({
-      ...data
-    });
-  });
 
   return (
-    <Appbar style={styles.appBar} theme={theme}>
-      <View pointerEvents={is_landing || is_home ? 'none' : null}>
-        <IconButton
-          icon='arrow-left'
-          theme={theme}
-          size={24}
-          color='rgb(255, 255, 255)'
-          onPress={() => nav('/landing')}
-          style={is_landing || is_home ? styles.hide_button : styles.show_button}
-        ></IconButton>
-      </View>
-      <Appbar.Content titleStyle={{ alignSelf: 'center' }} title="medicamina"></Appbar.Content>
-      <View pointerEvents={!is_login ? null : 'none'}>
-        {is_landing ?
-          (
-            <View>
-              <IconButton
-                icon='login'
-                // theme={theme}
-                color='rgb(255, 255, 255)'
-                size={24}
-                onPress={() => nav('/login')}
-                style={is_landing ? styles.show_button : styles.hide_button}
-              />
-            </View>
-          ) :
-          (
-            <View style={styles.row}>
-              <View>
-                <IconButton
-                  icon='bell'
-                  // theme={theme}
-                  color='rgb(255, 255, 255)'
-                  size={24}
-                  onPress={() => toggleNotifications()}
-                  style={is_landing || is_login ? styles.hide_button : styles.show_button}
-                />
-                {state.unread > 0 && (
-                  <Badge
-                    visible={(!is_login && !is_landing)}
-                    style={styles.badge}>
-                    {state.unread}
-                  </Badge>
-                )}
-              </View>
-            </View>
-          )}
-      </View>
-    </Appbar>
+    <></>
+    // <Appbar style={styles.appBar} theme={theme}>
+    //   <Appbar.Content titleStyle={{ alignSelf: 'flex-start' }} title="Stanley University"></Appbar.Content>
+    // </Appbar>
   );
 }
 
